c89e90f3e000dfce86e0e86739109a1aef7e7f3d
NguyenMinhTuan2906 <monteeladykillah@gmail.com>
2017-04-10 10:55:54 +0700

    Design home pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t ".food_drink"
+    if page_title.empty?
+      base_title
+    else
+      base_title
+    end
+  end
 end
